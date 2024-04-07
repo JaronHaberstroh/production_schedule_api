63b48cec528f2193e239f416c1fdc1bd4fdd0a70
JHSpaz988 <JHSpaz988@gmail.com>
2024-04-07 06:45:30 -0400

    Add custom AppError class

---
+++ __tests__/utils/appError.test.js
@@ -0,0 +1,29 @@
+import AppError from "#utils/appError.js";
+
+describe("Custom AppError", () => {
+  let message, statusCode;
+
+  beforeAll(() => {
+    // Set test variables
+    message = "test message";
+    statusCode = 404;
+  });
+
+  test("calling AppError creates new instance of AppError", () => {
+    // Create instance of AppError
+    const error = () => {
+      throw new AppError(message, statusCode);
+    };
+
+    // Expect calling AppError to create instance of AppError
+    expect(error).toThrowError(new AppError(message, statusCode));
+  });
+
+  test("should return message and status in error code", () => {
+    const error = new AppError(message, statusCode);
+
+    // Expect error to contain message and statusCode
+    expect(error.message).toBe(message);
+    expect(error.statusCode).toBe(statusCode);
+  });
+});

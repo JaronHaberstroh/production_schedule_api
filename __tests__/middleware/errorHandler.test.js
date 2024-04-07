9a17c494e2634c98d2f05e9ff2c3faef46e5f82c
JHSpaz988 <JHSpaz988@gmail.com>
2024-04-07 15:32:49 -0400

    Add error handling middleware

---
+++ __tests__/middleware/errorHandler.test.js
@@ -0,0 +1,41 @@
+// @vitest-environment express
+
+import errorHandler from "src/middleware/errorHandler.js";
+
+describe("errroHandler middleware", () => {
+  let mockError;
+  beforeAll(() => {
+    mockError = {
+      message: "test message",
+      statusCode: 404,
+      error: vi.fn(),
+      stack: vi.fn(),
+    };
+  });
+
+  beforeEach(() => {
+    mockRes.headerSent = false;
+  });
+
+  test("should do nothing if res.headerSent is true", () => {
+    mockRes.headerSent = true;
+
+    errorHandler(mockError, mockReq, mockRes, mockNext);
+
+    expect(mockNext).toBeCalledWith(mockError);
+  });
+
+  test("should send proper response", () => {
+    // Call errorHandler
+    errorHandler(mockError, mockReq, mockRes, mockNext);
+
+    // Expect res to contain all info for client
+    expect(mockRes.status).toBeCalledWith(404);
+    expect(mockRes.json).toBeCalledWith({
+      status: 404,
+      message: "test message",
+      error: mockError,
+      stack: mockError.stack,
+    });
+  });
+});

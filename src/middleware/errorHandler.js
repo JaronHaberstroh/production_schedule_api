b62ff70625a4167933ccc0166394c287e5c68671
JHSpaz988 <JHSpaz988@gmail.com>
2024-04-13 22:33:58 -0400

    Refactor middleware for readability and concise error handling

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -3,18 +3,10 @@ const errorHandler = (error, req, res, next) => {
     return next(error);
   }
 
-  if (process.env.NODE_ENV === "production") {
-    return res.status(error.statusCode || 500).json({
-      status: "error",
-      message: error.message || "An unknow error occured!",
-    });
-  }
-
   res.status(error.statusCode || 500).json({
     status: error.statusCode || 500,
     message: error.message || "An unknown error occured!",
-    error: error,
-    stack: error.stack,
+    stack: process.env.NODE_ENV === "development" || "test" ? error.stack : {},
   });
 };
 

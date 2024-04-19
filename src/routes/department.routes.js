04fb4868e5866a615c777f98e34f684c2af1d2d4
JHSpaz988 <JHSpaz988@gmail.com>
2024-04-19 04:19:33 -0400

    Add validation to department routes

--- src/routes/department.routes.js
+++ src/routes/department.routes.js
@@ -3,13 +3,32 @@ import createDepartment from "#controllers/department/createDepartment.js";
 import readDepartment from "#controllers/department/readDepartment.js";
 import updateDepartment from "#controllers/department/updateDepartment.js";
 import deleteDepartment from "#controllers/department/deleteDepartment.js";
+import validate, {
+  departmentIdValidator,
+  departmentNameValidator,
+} from "#controllers/department/validation.js";
 
 const router = Router();
 
-router.post("/create", createDepartment);
-router.get("/read/:_id", readDepartment);
-router.get("/read", readDepartment);
-router.patch("/update/:_id", updateDepartment);
-router.delete("/delete/:_id", readDepartment, deleteDepartment);
+router.post("/create", departmentNameValidator(), validate, createDepartment);
+
+router.get("/read/:_id", departmentIdValidator(), validate, readDepartment);
+
+router.get("/read", departmentNameValidator(), readDepartment);
+
+router.patch(
+  "/update/:_id",
+  departmentNameValidator(),
+  departmentIdValidator(),
+  validate,
+  updateDepartment
+);
+
+router.delete(
+  "/delete/:_id",
+  departmentIdValidator(),
+  validate,
+  deleteDepartment
+);
 
 export default router;

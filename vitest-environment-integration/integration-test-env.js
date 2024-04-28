078362eac958acb1da775d0ee48d595d9bee999a
JHSpaz988 <JHSpaz988@gmail.com>
2024-04-28 02:24:07 -0400

    Add seedDB function to populate DB for integration tests

--- vitest-environment-integration/integration-test-env.js
+++ vitest-environment-integration/integration-test-env.js
@@ -12,6 +12,7 @@ export default {
 
     globalThis.mongoConnection = mongoConnection;
     globalThis.mongoServer = mongoServer;
+    globalThis.seedDB = seedDB;
     return {
       async teardown() {
         // Disconnect from DB
@@ -19,4 +20,40 @@ export default {
       },
     };
   },
+};
+
+const seedDB = async (models) => {
+  const { Department, ProductionLine } = models;
+
+  let departments;
+  if (Department) {
+    departments = Array.from(
+      { length: 5 },
+      (_, idx) =>
+        new Department({
+          departmentName: `Department_${idx + 1}`,
+          productionLines: [],
+        })
+    );
+  }
+
+  if (ProductionLine) {
+    for (let i = 0; i < departments.length; i++) {
+      const productionLines = Array.from(
+        { length: 5 },
+        (_, idx) =>
+          new ProductionLine({
+            lineName: `ProductionLine_${idx + 1}`,
+            department: departments[i]._id,
+          })
+      );
+      const savedProductionLines = await ProductionLine.insertMany(
+        productionLines
+      );
+      departments[i].productionLines.push(
+        ...savedProductionLines.map((line) => line._id)
+      );
+    }
+  }
+  await Department.insertMany(departments);
 };

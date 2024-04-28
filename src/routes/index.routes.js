4ef786732fb1e727ea55d32f081bce75c9abb6c1
JHSpaz988 <JHSpaz988@gmail.com>
2024-04-28 02:32:25 -0400

    Add productionLineRoutes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import departmentRoutes from "./department.routes.js";
-
+import productionLineRoutes from "./productionLine.routes.js";
 const router = Router();
 
-router.use("/department", departmentRoutes);
+router.use("/departments", departmentRoutes);
+router.use("/departments/:departmentId/production-lines", productionLineRoutes);
 
 export default router;

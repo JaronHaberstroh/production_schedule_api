53881167efe3ac53ec5aaa266dd4dff116f1f0ab
JHSpaz988 <JHSpaz988@gmail.com>
2024-04-28 23:47:21 -0400

    Add route for production line CRUD operations

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,9 +2,10 @@ import { Router } from "express";
 
 import departmentRoutes from "./department.routes.js";
 import productionLineRoutes from "./productionLine.routes.js";
+
 const router = Router();
 
 router.use("/departments", departmentRoutes);
-router.use("/departments/:departmentId/production-lines", productionLineRoutes);
+router.use("/departments", productionLineRoutes);
 
 export default router;

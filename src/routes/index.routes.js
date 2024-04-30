de1e64640491e8343caea517cbb6170a57bc4388
JHSpaz988 <JHSpaz988@gmail.com>
2024-04-30 04:33:07 -0400

    Add work-positions route

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-
 import departmentRoutes from "./department.routes.js";
 import productionLineRoutes from "./productionLine.routes.js";
+import workPositionRoutes from "./workPosition.routes.js";
 
 const router = Router();
 
 router.use("/departments", departmentRoutes);
 router.use("/departments", productionLineRoutes);
+router.use("/work-postions", workPositionRoutes);
 
 export default router;

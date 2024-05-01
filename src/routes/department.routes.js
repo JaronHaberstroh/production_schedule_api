da7dba8e1ee349d5ccbe9f4146aa3b0dee8f5055
JHSpaz988 <JHSpaz988@gmail.com>
2024-05-01 03:26:43 -0400

    Update routes to use new validation functions

--- src/routes/department.routes.js
+++ src/routes/department.routes.js
@@ -3,27 +3,26 @@ import createDepartment from "#controllers/department/createDepartment.js";
 import readDepartment from "#controllers/department/readDepartment.js";
 import updateDepartment from "#controllers/department/updateDepartment.js";
 import deleteDepartment from "#controllers/department/deleteDepartment.js";
-import validate, {
-  departmentIdValidator,
-  departmentNameValidator,
-} from "#controllers/department/validation.js";
+import { validate, checkId, checkName } from "./validation.js";
+import Department from "#models/department.js";
 
 const router = Router();
 
-router.post("/", departmentNameValidator(), validate, createDepartment);
+router.post(
+  "/",
+  [checkName("departmentName", Department), validate],
+  createDepartment
+);
 
-router.get("/:_id", departmentIdValidator(), validate, readDepartment);
-
+router.get("/:_id", [checkId(Department), validate], readDepartment);
 router.get("/", readDepartment);
 
 router.patch(
   "/:_id",
-  departmentNameValidator(),
-  departmentIdValidator(),
-  validate,
+  [checkId(Department), checkName("departmentName", Department), validate],
   updateDepartment
 );
 
-router.delete("/:_id", departmentIdValidator(), validate, deleteDepartment);
+router.delete("/:_id", [checkId(Department), validate], deleteDepartment);
 
 export default router;

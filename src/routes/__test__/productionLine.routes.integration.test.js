3268490c03664321eb785deb8be8ebb2ee49e34f
JHSpaz988 <JHSpaz988@gmail.com>
2024-05-01 01:59:16 -0400

    Add hooks to seed/drop DB by calling test routes

--- src/routes/__test__/productionLine.routes.integration.test.js
+++ src/routes/__test__/productionLine.routes.integration.test.js
@@ -1,22 +1,27 @@
 import request from "supertest";
-import Department from "#models/department.js";
-import ProductionLine from "#models/productionLine.js";
 import app from "src/app.js";
+import Department from "#models/department.js";
 
 describe("Production line routes", () => {
   let route;
+
   beforeEach(async () => {
-    // Seed DB
-    const seedParams = { Department, ProductionLine };
-    await seedDB(seedParams);
+    const result = await request(app).post("/api/test/seedDB");
+    console.log(result.body.message);
 
-    const department = await Department.find();
-    const departmentId = department[0].id;
-
-    route = `/api/departments/${departmentId}/production-lines/`;
+    const department = await Department.findOne();
+    route = `/api/departments/${department.id}/production-lines/`;
   });
 
-  afterEach(() => {});
+  afterEach(async () => {
+    const result = await request(app).delete("/api/test/dropDB");
+    console.log(result.body.message);
+  });
+
+  afterAll(async () => {
+    const result = await request(app).delete("/api/test/dropDB");
+    console.log("DropDB after all tests finish", result.body.message);
+  });
 
   test("should create a new productionLine and update it's department productionLine list", async () => {
     const newProductionLine = {

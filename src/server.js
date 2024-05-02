66c8a3a18000cdddabd04d831c8a2bc57c02bb95
JHSpaz988 <JHSpaz988@gmail.com>
2024-05-02 11:44:40 -0400

    Remove console.log()

--- src/server.js
+++ src/server.js
@@ -8,8 +8,6 @@ const DB_URI = process.env.MONGO_URI;
 // Connect mongoDB
 await connectDB();
 
-console.log(`Connected to DB ${DB_URI}`);
-
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });

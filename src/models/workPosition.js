685b06da3f485c39280a79bbe39a92bdda38cd92
JHSpaz988 <JHSpaz988@gmail.com>
2024-05-03 05:33:30 -0400

    Update schema name to be singular

--- src/models/workPosition.js
+++ src/models/workPosition.js
@@ -1,12 +1,12 @@
 import { Schema, model } from "mongoose";
 
-export const workPositionsSchema = new Schema({
+export const workPositionSchema = new Schema({
   positionName: {
     type: String,
     required: [true, "positionName is required"],
   },
 });
 
-const WorkPosition = model("WorkPosition", workPositionsSchema);
+const WorkPosition = model("WorkPosition", workPositionSchema);
 
 export default WorkPosition;

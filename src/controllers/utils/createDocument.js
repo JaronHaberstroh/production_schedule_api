4c07a65a4229d2de25b4a67b2b8c9a493f8c3eac
JHSpaz988 <JHSpaz988@gmail.com>
2024-05-07 07:23:04 -0400

    Add session support to createDocument function

--- src/controllers/utils/createDocument.js
+++ src/controllers/utils/createDocument.js
@@ -1,11 +1,11 @@
 import { errorResponse, successResponse } from "#responses/response.js";
 
-const createDocument = async (model, params) => {
+const createDocument = async (model, params, session = undefined) => {
   const newDocument = new model(params);
 
   let savedDocument;
   try {
-    savedDocument = await newDocument.save();
+    savedDocument = await newDocument.save({ session });
   } catch (error) {
     return errorResponse(
       `Error while saving new document: ${error.message}`,

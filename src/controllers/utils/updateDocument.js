9d52ae6d51e265098b1f56916c3419bc9b992f2b
JHSpaz988 <JHSpaz988@gmail.com>
2024-05-08 15:02:06 -0400

    Refactor: Adjust logic to work with mongoose sessions

--- src/controllers/utils/updateDocument.js
+++ src/controllers/utils/updateDocument.js
@@ -1,25 +1,32 @@
 import { successResponse, errorResponse } from "#responses/response.js";
 
-const updateDocumment = async (model, params) => {
-  let result;
+const updateDocumment = async (model, params, session = undefined) => {
+  const { query, params: updateParams } = params;
+
   try {
-    result = await model.updateOne({ _id: params.query }, params.params);
+    const foundModel = await model.findById(query);
+    if (!foundModel) {
+      return errorResponse(`Failed to locate document matching query`, 404);
+    }
+
+    const updatedModel = Object.assign(foundModel, updateParams);
+
+    const savedDocument = await updatedModel.save({ session });
+    return handleSavedDocument(savedDocument);
   } catch (error) {
     return errorResponse(
       `Error updating document: ${error.message}`,
       error.statusCode || 500
     );
   }
-
-  if (!result.matchedCount) {
-    return errorResponse(`Failed to locate document matching query`, 404);
-  }
-
-  if (!result.modifiedCount) {
-    return errorResponse(`No document modified`, 500);
-  }
-
-  return successResponse(`Successfully updated document`, 200, result);
 };
 
 export default updateDocumment;
+
+const handleSavedDocument = (savedDocument) => {
+  if (!savedDocument) {
+    return errorResponse(`Document not modified`, 500);
+  }
+
+  return successResponse(`Successfully updated document`, 200, savedDocument);
+};

113274c0a5a8ff60985c10e2af1ef07edfb03205
JHSpaz988 <JHSpaz988@gmail.com>
2024-05-22 03:43:15 -0400

    Add eslint

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import vitest from "eslint-plugin-vitest";
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    files: ["**/*.test.js"],
+    plugins: { vitest },
+    rules: {
+      ...vitest.configs.recommended.rules,
+    },
+    languageOptions: {
+      globals: {
+        ...vitest.environments.env.globals,
+        mockReq: "writable",
+        mockRes: "writable",
+        mockNext: "writable",
+        testModel: "writable",
+        testData: "writable",
+      },
+    },
+  },
+];

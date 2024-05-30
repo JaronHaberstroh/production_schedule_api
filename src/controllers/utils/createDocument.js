8e46be689a7c671cb7179acfa4d565cd38658263
JHSpaz988 <JHSpaz988@gmail.com>
2024-05-30 09:55:05 -0400

    Refactor: extract saveDocument() into its own fn

--- src/controllers/utils/createDocument.js
+++ src/controllers/utils/createDocument.js
@@ -1,19 +1,5 @@
-import { errorResponse, successResponse } from "#responses/response.js";
-
-const createDocument = async (model, params, session = undefined) => {
-  const newDocument = new model(params);
-
-  let savedDocument;
-  try {
-    savedDocument = await newDocument.save({ session });
-  } catch (error) {
-    return errorResponse(
-      `Error while saving new document: ${error.message}`,
-      error.statusCode || 500
-    );
-  }
-
-  return successResponse(`Successfully saved new document`, 201, savedDocument);
+const createDocument = (model, params) => {
+  return new model(params);
 };
 
 export default createDocument;

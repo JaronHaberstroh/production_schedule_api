dfdabf625cd035a2005195610e9699fa2323a8a0
JHSpaz988 <JHSpaz988@gmail.com>
2024-05-31 14:56:14 -0400

    Refactor: reduce function to single purpose and update testing

--- src/controllers/utils/updateDocument.js
+++ src/controllers/utils/updateDocument.js
@@ -1,32 +1,5 @@
-import { successResponse, errorResponse } from "#responses/response.js";
-
-const updateDocumment = async (model, params, session = undefined) => {
-  const { query, params: updateParams } = params;
-
-  try {
-    const foundModel = await model.findById(query);
-    if (!foundModel) {
-      return errorResponse(`Failed to locate document matching query`, 404);
-    }
-
-    const updatedModel = Object.assign(foundModel, updateParams);
-
-    const savedDocument = await updatedModel.save({ session });
-    return handleSavedDocument(savedDocument);
-  } catch (error) {
-    return errorResponse(
-      `Error updating document: ${error.message}`,
-      error.statusCode || 500
-    );
-  }
+const updateDocumment = (document, params) => {
+  return Object.assign(document, params);
 };
 
 export default updateDocumment;
-
-const handleSavedDocument = (savedDocument) => {
-  if (!savedDocument) {
-    return errorResponse(`Document not modified`, 500);
-  }
-
-  return successResponse(`Successfully updated document`, 200, savedDocument);
-};

40f0f077ccc15c0237f84627f72c3697d14986ce
JHSpaz988 <JHSpaz988@gmail.com>
2024-06-04 02:20:06 -0400

    Refactor: update controller and tests making them more robust and concise

--- src/controllers/productionLine/createProductionLine.js
+++ src/controllers/productionLine/createProductionLine.js
@@ -1,3 +1,6 @@
+import findDocumentById from "#controllers/utils/findDocumentById";
+import createDocument from "#controllers/utils/createDocument";
+import saveDocument from "#controllers/utils/saveDocument";
 import Department from "#models/department.js";
 import ProductionLine from "#models/productionLine.js";
 import { successResponse } from "#responses/response.js";
@@ -5,37 +8,66 @@ import AppError from "#utils/appError.js";
 
 const createProductionLine = async (req, res, next) => {
   const { departmentId } = req.params;
-  const { lineName } = req.body;
+  const productionLineData = req.body;
   const session = req.session;
 
-  let savedProductionLine;
   try {
-    // Add production line
-    const params = { lineName, department: departmentId };
-    const productionLine = new ProductionLine(params);
-    savedProductionLine = await productionLine.save({ session });
+    // Create and save production line
+    const productionLineDocument = createDocument(
+      ProductionLine,
+      productionLineData,
+    );
+    const savedProductionLine = await saveDocument(
+      productionLineDocument,
+      session,
+    );
+    if (!savedProductionLine.success) {
+      return next(
+        new AppError(
+          `Failed to save production line: ${savedProductionLine.message}`,
+          savedProductionLine.statusCode || 500,
+        ),
+      );
+    }
 
-    // Update Department
-    const department = await Department.findOne({ _id: departmentId });
-    department.productionLines.push(savedProductionLine._id);
-    await department.save({ session });
+    // Update and save department
+    const foundDepartment = await findDocumentById(Department, departmentId);
+    if (!foundDepartment) {
+      return next(
+        new AppError(
+          `Failed to find department: ${foundDepartment.message}`,
+          foundDepartment.statusCode || 500,
+        ),
+      );
+    }
+    const departmentDocument = foundDepartment.data;
+    departmentDocument.productionLines.push(productionLineDocument);
+    const savedDepartment = await saveDocument(departmentDocument, session);
+    if (!savedDepartment.success) {
+      return next(
+        new AppError(
+          `Failed to save department: ${savedDepartment.message}`,
+          savedDepartment.statusCode || 500,
+        ),
+      );
+    }
+
+    res
+      .status(201)
+      .json(
+        successResponse(
+          `Production line created successfully`,
+          201,
+          savedProductionLine,
+        ),
+      );
   } catch (error) {
     const err = new AppError(
       `Unhandled Exception: ${error.message}`,
-      error.statusCode || 500
+      error.statusCode || 500,
     );
     return next(err);
   }
-
-  res
-    .status(201)
-    .json(
-      successResponse(
-        `Production line created successfully`,
-        201,
-        savedProductionLine
-      )
-    );
 };
 
 export default createProductionLine;
